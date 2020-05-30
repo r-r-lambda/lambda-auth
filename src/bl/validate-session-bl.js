9a2bbd49e88f51147fa0838dc12d4b521a2c9933
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-05-30 18:18:36 -0500

    FEAT: Bussiness logic for sesion validation

---
+++ src/bl/validate-session-bl.js
@@ -0,0 +1,25 @@
+const validateSessionDao = require('../dao/validate-session-dao');
+
+const validateSessionBL = async (token) => {
+  const userData = validateSessionDao(token);
+
+  const authPolicy = {
+    principalId: 'id_principal',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+        },
+      ],
+      context: {
+        userInfo: userData,
+      },
+    },
+  };
+
+  return authPolicy;
+};
+
+module.exports = validateSessionBL;
