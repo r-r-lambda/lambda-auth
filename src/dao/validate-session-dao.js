a53f72ad85df59b4c71e14ec1fd1a747147a6e9a
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-06-04 15:57:25 -0500

    REFACTOR: Use exported instance of firebase from auth-config

--- src/dao/validate-session-dao.js
+++ src/dao/validate-session-dao.js
@@ -1,15 +1,12 @@
 const authConfig = require('../config/auth-config');
-const firebase = require('firebase-admin');
 
 const AuthorizationError = require('../errors/authorization-error');
 
 const validateSessionDao = async (token) => {
-  firebase.initializeApp(authConfig);
-
   let dataFromToken;
 
   try {
-    dataFromToken = await firebase.auth().verifyIdToken(token);
+    dataFromToken = await authConfig.auth().verifyIdToken(token);
   } catch (error) {
     throw new AuthorizationError(error);
   }
