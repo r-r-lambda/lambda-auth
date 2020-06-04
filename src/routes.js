5e4052c2c33456386f1614ef618cea9d3fde21f4
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-06-04 16:24:41 -0500

    REFACTOR: remove express logic to convert to lambda function without express

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const validateSession = require('./services/validate-session');
-
-const router = express.Router();
-
-router.get('/', validateSession);
-
-module.exports = router;
